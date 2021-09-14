dbc02aa33e1e8e11a85c121a319ecc7f95f278c4
Henry Chan <henry2014ips@gmail.com>
2021-09-14 22:50:06 +0800

    Finished Day0 Hello World

---
+++ src/day0/index.js
@@ -0,0 +1,22 @@
+'use strict';
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+var inputString = '';
+var inputLines = [];
+var currentLine = 0;
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+process.stdin.on('end', function () {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+function readLine() {
+    return inputLines[currentLine++];
+}
+function main() {
+    // Enter your code here
+    console.log("Hello, World.");
+    console.log(inputLines[0]);
+}
